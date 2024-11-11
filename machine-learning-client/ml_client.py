5f94957074544bd332a41a12722346e239625dd6
Applejam-ovo <ql2138@nyu.edu>
2024-11-11 03:16:53 -0500

    Solve pylint problems

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -1,7 +1,13 @@
-import cv2
+"""
+Machine learning client to process images and detect emotions.
+"""
+
+# pylint: disable=no-member
+
 import base64
+import cv2
 import numpy as np
-from fer import FER 
+from fer import FER
 from pymongo import MongoClient
 
 client = MongoClient("=created by teammates")
@@ -9,12 +15,16 @@ db = client["ml_database"]
 collection = db["analysis_results"]
 
 emotion_detector = FER()
-face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 # get image from mongoDB
 # might change due to backend works.
 def decode_image(image_data):
+    """
+    Decode the image from databse
+    """
     image_bytes = base64.b64decode(image_data)
     nparr = np.frombuffer(image_bytes, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # pylint: disable=no-member
     return img
